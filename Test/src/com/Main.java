0bc2225721497b2d7efc284ee8426269cc97627a
labdemo80 <labdemo80@gmail.com>
2026-01-21 15:12:28 +0530

    2nd commit

--- Test/src/com/Main.java
+++ Test/src/com/Main.java
@@ -2,6 +2,9 @@ package com;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("1234");
+		System.out.println("Hello User");
+		int a=10;
+		System.out.println(a);
 	}
 }
